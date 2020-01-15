a6b4dd5609c17c20baee9b7a3e3a40bbfc9893ec
Jaehong Kang <j.kang@ridi.com>
2020-01-15 12:08:36 +0900

    Fixes test for Swift 5.0

--- Tests/SNFoundationTests/SNFoundationTests.swift
+++ Tests/SNFoundationTests/SNFoundationTests.swift
@@ -18,9 +18,15 @@ final class SNFoundationTests: XCTestCase {
         XCTAssertNotNil(Unknownable<A>.self)
     }
 
+    #if swift(>=5.1)
     func testWeak() {
         XCTAssertNotNil(Weak<AnyObject>.self)
     }
+
+    func testPurgable() {
+        XCTAssertNotNil(Purgeable<Any>.self)
+    }
+    #endif
 
     static var allTests = [
         ("testExpressional", testExpressional),
