b6020a81ce1d4897a21e2b17cdaac07648f61cbf
Jaehong Kang <j.kang@ridi.com>
2020-01-16 16:18:55 +0900

    Remove SNFoundationTests

--- Tests/SNFoundationTests/XCTestManifests.swift
+++ Tests/SNFoundationTests/XCTestManifests.swift
@@ -3,7 +3,6 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(SNFoundationTests.allTests),
         testCase(NotificationCenterTests.allTests),
         testCase(LazyTests.allTests),
         testCase(UnknownableTests.allTests),
